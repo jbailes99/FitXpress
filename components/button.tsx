9c8619915135c98bbfeb76e56baaeaa753cf9ab0
Jacob Bailes <contact@jbailes.com>
2024-10-23 04:04:16 -0400

    try fix 2

--- components/button.tsx
+++ components/button.tsx
@@ -1,6 +1,5 @@
 import { memo } from 'react'
 import cn from 'classnames'
-import { Button as MaterialButton } from '@material-tailwind/react'
 
 export const Button = memo(function Button({
   children,
@@ -8,16 +7,14 @@ export const Button = memo(function Button({
   className,
   rounded,
   shadow,
-  color,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & {
   className?: string
   rounded?: boolean
   shadow?: boolean
-  color?: 'primary' | 'secondary' | 'default'
 }) {
   return (
-    <MaterialButton
+    <button
       className={cn(
         className,
         'px-4 py-2 font-medium text-white transition-all disabled:opacity-30 disabled:cursor-not-allowed',
@@ -26,12 +23,11 @@ export const Button = memo(function Button({
           'shadow-lg': shadow,
         }
       )}
-      color="purple"
       onClick={onClick}
       {...props}
     >
       {children}
-    </MaterialButton>
+    </button>
   )
 })
 
